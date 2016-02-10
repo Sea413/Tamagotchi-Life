dd3e6252491062e44ec493a64ac00388adb1558a
TeamC# <TeamC#>
2016-02-10 13:00:26 -0800

    initial commit

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -4,36 +4,14 @@ using System;
 using RectangleStuff;
 using CubeNamespace;
 
-
-
-
-namespace shapeNamespace
+namespace Tamagotchi
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
-      Get["/"] =_=> {
-        return View ["rectangle_form.cshtml"];
-      };
-      // Get ["/rectangle_result"]=_=>{
-      //   Rectangle myRectangle = new Rectangle (Request.Query ["side-length"], Request.Query["side-width"]);
-      //   return View ["rectangle_result.cshtml", myRectangle];
-      // };
-      Get["/rectangle_result"]=_=>  {
-        Dictionary <string, object> whistle = new Dictionary <string,object>();
 
-        Rectangle myownRectangle = new Rectangle(Request.Query["side-length"],Request.Query["side-width"]);
 
-        whistle.Add("ResultingRectangle", myownRectangle);
-
-        if (myownRectangle.Issquare()) {
-          Cube myCube = new Cube (myownRectangle);
-          whistle.Add("ResultingCube", myCube);
-          Console.WriteLine(whistle);
-        }
-        return View["rectangle_result.cshtml",whistle];
-      };
     }
   }
 }
