0da7f9a8af000324b1a0b4f54339523fe67e4c35
TeamC# <TeamC#>
2016-02-10 10:53:17 -0800

    route functionality

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -1,30 +1,19 @@
 using Nancy;
 using System.Collections.Generic;
-using Todo.Objects;
-using System;
 
-namespace todoNamespace
+
+namespace RectangleStuff
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
       Get["/"] =_=> {
-        return View ["add_view.cshtml"];
+        return View ["rectangle_form.cshtml"];
       };
-      Get["/task/{id}"] = parameters => {
-        Task newTask = Task.Find(parameters.id);
-        return View["/task.cshtml", newTask];
-      };
-      Post["/view_all"] =_=> {
-        Task newTask = new Task(Request.Form["add"]);
-        // newTask.Save();
-        List<Task> _superlist = Task.GetAll();
-        return View ["view_all.cshtml", _superlist];
-      };
-      Get["/view_all_posts"] =_=> {
-        List<Task> _superlist = Task.GetAll();
-        return View ["view_all_posts.cshtml", _superlist];
+      Get ["/rectangle_result"]=_=>{
+        Rectangle myRectangle = new Rectangle (Request.Query ["side-length"], Request.Query["side-width"]);
+        return View ["rectangle_result.cshtml", myRectangle];
       };
     }
   }
