5789d4eec78c690346f9b8c968308d982629fe32
TeamC# <TeamC#>
2016-02-10 11:37:32 -0800

    Add all rectangle hw files.

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -1,8 +1,13 @@
 using Nancy;
 using System.Collections.Generic;
+using System;
+using RectangleStuff;
+using CubeNamespace;
 
 
-namespace RectangleStuff
+
+
+namespace shapeNamespace
 {
   public class HomeModule : NancyModule
   {
@@ -11,9 +16,23 @@ namespace RectangleStuff
       Get["/"] =_=> {
         return View ["rectangle_form.cshtml"];
       };
-      Get ["/rectangle_result"]=_=>{
-        Rectangle myRectangle = new Rectangle (Request.Query ["side-length"], Request.Query["side-width"]);
-        return View ["rectangle_result.cshtml", myRectangle];
+      // Get ["/rectangle_result"]=_=>{
+      //   Rectangle myRectangle = new Rectangle (Request.Query ["side-length"], Request.Query["side-width"]);
+      //   return View ["rectangle_result.cshtml", myRectangle];
+      // };
+      Get["/rectangle_result"]=_=>  {
+        Dictionary <string, object> whistle = new Dictionary <string,object>();
+
+        Rectangle myownRectangle = new Rectangle(Request.Query["side-length"],Request.Query["side-width"]);
+
+        whistle.Add("ResultingRectangle", myownRectangle);
+
+        if (myownRectangle.Issquare()) {
+          Cube myCube = new Cube (myownRectangle);
+          whistle.Add("ResultingCube", myCube);
+          Console.WriteLine(whistle);
+        }
+        return View["rectangle_result.cshtml",whistle];
       };
     }
   }
